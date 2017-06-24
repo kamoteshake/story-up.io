4074fdf48ead9f997f352146d9cc885627f6349c
Ray Agor <kamoteshake@gmail.com>
2017-06-24 00:14:36 -0500

    fixed some syntax errors on includes. I was durping so hard

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -2,7 +2,7 @@
   // DB connection must be in here.
 
   // Global Functions
-  include __DIR__.'/function.php';
+  include __DIR__.'/function.inc.php';
 
   // Website Configs
   $WebTitle = 'Story Up';
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include __DIR__.'/inc/header.inc.php;' ?>
+<?php include __DIR__.'/inc/header.inc.php'; ?>
 <?php
   echo '<div>Hello World!</div>';
 ?>
